4f1821893b4203213e83550500b9adb64d19fb3c
ialarcon <gabriel.2015ap@gmail.com>
2022-12-06 19:26:28 -0500

    Adding some comments

--- mapa.py
+++ mapa.py
@@ -1,9 +1,12 @@
+#Bibliotecas
 import geopandas
 import matplotlib.pyplot as plt
 
+#Guardando los datos que usaremos
 path = geopandas.datasets.get_path("naturalearth_lowres")
 world = geopandas.read_file(path)
 
+#Graficar nuestro mapa
 ax = world.plot()
 world[world.continent == 'South America'].plot(ax = ax, cmap = 'Greens')
 plt.show()
